0c25e126eb70e21a71ef80ffba90e0abb8c369da
mertoalex <61164301+mertoalex@users.noreply.github.com>
2022-03-11 02:06:09 +0300

    Update kreosus.js

--- kreosus.js
+++ kreosus.js
@@ -5,6 +5,7 @@ js = d.createElement(s);
 js.id = id;
 js.src = 'https://kreosus.com/public/iframe/js/iframe-api.js';
 kjs.parentNode.insertBefore(js, kjs);
-document.getElementById('kreosus-iframe').style.borderRadius = "25px" // w3c
-document.getElementById('kreosus-iframe').style.MozBorderRadius = "25px" // mozilla
+if($.browser.mozilla){// mozilla
+document.getElementById('kreosus-iframe').style.MozBorderRadius = "25px"}else{ 
+document.getElementById('kreosus-iframe').style.borderRadius = "25px"} // w3c
 }(document, 'script', 'kreosus-iframe-api'));
